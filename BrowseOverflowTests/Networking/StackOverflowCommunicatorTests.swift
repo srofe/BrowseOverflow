4c14d4443be8c45fbd2e4016531f9bd617c67624
Simon Rofe <simon.rofe@ieee.org>
2019-06-18 15:44:57 +1000

    Move test class into its own file

--- BrowseOverflowTests/Networking/StackOverflowCommunicatorTests.swift
+++ BrowseOverflowTests/Networking/StackOverflowCommunicatorTests.swift
@@ -121,28 +121,5 @@ class StackOverflowCommunicatorTests: XCTestCase {
         communicator.downloadAnswersToQuestion(with: sutQuestionId)
         let manager = communicator.delegate as? MockStackOverflowManager
         XCTAssertEqual(manager?.topicFailureErrorCode, 404)
-
-    }
-}
-
-class IntrospectionStackOverflowCommunicator: StackOverflowCommunicator {
-
-    private func fetchContentAtUrl(with text: String) {
-        let dataTask = MockDataTask()
-        dataTask.statusCode = 404
-        self.urlSession(self.session as! URLSession, task: dataTask, didCompleteWithError: nil)
-    }
-
-    override func searchForQuestions(with tag: String) {
-        guard let encodedTag = tag.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { fatalError() }
-        fetchContentAtUrl(with: "https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&tagged=\(encodedTag)&site=stackoverflow")
-    }
-
-    override func downloadInformationForQuestion(with id: Int) {
-        fetchContentAtUrl(with: "https://api.stackexchange.com/2.2/questions/\(id)?order=desc&sort=activity&site=stackoverflow&filter=withBody")
-    }
-
-    override func downloadAnswersToQuestion(with id: Int) {
-        fetchContentAtUrl(with: "https://api.stackexchange.com/2.2/questions/\(id)/answers?order=desc&sort=activity&site=stackoverflow")
     }
 }
